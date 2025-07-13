c6b89f6f3720e60d4ae63920eec1ab53f5e04e17
lilipato <canoy.alliyahmae1@gmail.com>
2025-07-14 01:51:10 +0800

    updated build v3

--- vite.config.ts
+++ vite.config.ts
@@ -7,24 +7,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/css/app.css', 
-                'resources/js/app.tsx',
-                'resources/js/pages/home.tsx',
-                'resources/js/pages/about.tsx',
-                'resources/js/pages/contact.tsx',
-                'resources/js/pages/menu.tsx',
-                'resources/js/pages/reservation.tsx',
-                'resources/js/pages/checkout.tsx',
-                'resources/js/pages/transaction.tsx',
-                'resources/js/pages/auth/login.tsx',
-                'resources/js/pages/admin/dashboard.tsx',
-                'resources/js/pages/admin/booking.tsx',
-                'resources/js/pages/admin/setting.tsx',
-                'resources/js/pages/admin/transactions.tsx',
-                'resources/js/pages/staff/dashboard.tsx',
-                'resources/js/pages/404.tsx'
-            ],
+            input: ['resources/css/app.css', 'resources/js/app.tsx'],
             ssr: 'resources/js/ssr.tsx',
             refresh: true,
         }),
